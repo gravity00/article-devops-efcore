05b6158b3ad7d04c3c3cf9caba081f0c28600a3a
João Simões <gravity00@users.noreply.github.com>
2023-12-01 11:59:38 +0000

    initial host setup

--- src/ArticleDevOpsEfCore.Database.Migrator/Program.cs
+++ src/ArticleDevOpsEfCore.Database.Migrator/Program.cs
@@ -1,2 +1,40 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using ArticleDevOpsEfCore.Database;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+
+var builder = Host.CreateApplicationBuilder(args);
+
+ConfigureLogging(
+    builder.Logging
+);
+
+ConfigureServices(
+    builder.Services,
+    builder.Configuration
+);
+
+using var host = builder.Build();
+
+return;
+
+static void ConfigureLogging(
+    ILoggingBuilder logging
+)
+{
+    // configure other logging providers, like NLog, Serilog or even Application Insights
+}
+
+static void ConfigureServices(
+    IServiceCollection services,
+    IConfiguration configuration
+)
+{
+    var connectionString = configuration.GetConnectionString("ArticleDevOpsEfCore");
+    services.AddDbContext<DatabaseContext>(options => options.UseSqlServer(
+        connectionString,
+        o => o.MigrationsAssembly(typeof(Program).Assembly.FullName)
+    ), ServiceLifetime.Singleton);
+}
