3fbd5dc5e85a5e110d71ba58ffc911c08438ee7b
João Simões <gravity00@users.noreply.github.com>
2023-12-01 02:46:11 +0000

    missing constructor for configuring the database context

--- src/ArticleDevOpsEfCore.Database/DatabaseContext.cs
+++ src/ArticleDevOpsEfCore.Database/DatabaseContext.cs
@@ -4,6 +4,11 @@ namespace ArticleDevOpsEfCore.Database;
 
 public class DatabaseContext : DbContext
 {
+    public DatabaseContext(DbContextOptions<DatabaseContext> options) : base(options)
+    {
+
+    }
+
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
         base.OnModelCreating(modelBuilder);
