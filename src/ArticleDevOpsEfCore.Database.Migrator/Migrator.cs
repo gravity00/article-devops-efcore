fef4d77856a5304aee115ec9006c9fcdd4d7441f
João Simões <gravity00@users.noreply.github.com>
2023-12-04 11:03:36 +0000

    implemented seed logic

--- src/ArticleDevOpsEfCore.Database.Migrator/Migrator.cs
+++ src/ArticleDevOpsEfCore.Database.Migrator/Migrator.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using ArticleDevOpsEfCore.Database.DataSeeds;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Logging;
 // ReSharper disable InconsistentNaming
 // ReSharper disable SuggestBaseTypeForParameterInConstructor
@@ -7,7 +8,8 @@ namespace ArticleDevOpsEfCore.Database;
 
 public class Migrator(
     ILogger<Migrator> _logger,
-    DatabaseContext _context
+    DatabaseContext _context,
+    IEnumerable<IDataSeed> _dataSeeds
 )
 {
     public async ValueTask RunAsync(CancellationToken ct)
@@ -15,5 +17,26 @@ public class Migrator(
         _logger.LogDebug("Migrating database to the latest version");
         await _context.Database.MigrateAsync(ct);
         _logger.LogInformation("Database migrated to latest version");
+
+        var seededOn = DateTimeOffset.UtcNow;
+        await using var tx = await _context.Database.BeginTransactionAsync(ct);
+
+        foreach (var dataSeed in _dataSeeds)
+        {
+            using var _ = _logger.BeginScope("DataSeed:{DataSeed}", dataSeed.GetType().Name);
+
+            _logger.LogDebug("Seeding data");
+
+            await dataSeed.SeedAsync(
+                "Migrator",
+                seededOn,
+                ct
+            );
+            await _context.SaveChangesAsync(ct);
+
+            _logger.LogInformation("Data was seeded");
+        }
+
+        await tx.CommitAsync(ct);
     }
 }
